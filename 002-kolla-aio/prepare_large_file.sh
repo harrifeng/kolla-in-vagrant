97187ffb7c51d46532bae8fc6947494acc364793
Haoran Feng <haoran.feng@alo7.com>
2017-08-17 14:36:43 +0800

    speed up the deploy

--- 002-kolla-aio/prepare_large_file.sh
+++ 002-kolla-aio/prepare_large_file.sh
@@ -2,5 +2,12 @@ FILE='docker-ce-17.06.0.ce-1.el7.centos.x86_64.rpm'
 if [ -f $FILE ]; then
     echo "$FILE is already here"
 else
-    wget https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-17.06.0.ce-1.el7.centos.x86_64.rpm
+    wget "https://download.docker.com/linux/centos/7/x86_64/stable/Packages/$FILE"
+fi
+
+FILE2='cirros-0.3.4-x86_64-disk.img'
+if [ -f $FILE2 ]; then
+    echo "$FILE is already here"
+else
+    wget "http://download.cirros-cloud.net/0.3.4/$FILE2"
 fi
