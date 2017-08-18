6be4712d826513ed5dbafb5e293911707da10e84
Haoran Feng <haoran.feng@alo7.com>
2017-08-18 11:06:26 +0800

    no rpm needed

--- 003-kolla-mul-deploy/prepare_large_file.sh
+++ 003-kolla-mul-deploy/prepare_large_file.sh
@@ -1,10 +1,3 @@
-FILE='docker-ce-17.06.0.ce-1.el7.centos.x86_64.rpm'
-if [ -f $FILE ]; then
-    echo "$FILE is already here"
-else
-    wget "https://download.docker.com/linux/centos/7/x86_64/stable/Packages/$FILE"
-fi
-
 FILE2='cirros-0.3.4-x86_64-disk.img'
 if [ -f $FILE2 ]; then
     echo "$FILE is already here"
