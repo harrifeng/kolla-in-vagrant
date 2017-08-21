5ea2f5f68d6cc5d5bdff99ce32eec6c3ae36aa52
Haoran Feng <haoran.feng@alo7.com>
2017-08-21 14:19:45 +0800

    improve

--- 003-kolla-mul-deploy/tools/prepare_large_file.sh
+++ 003-kolla-mul-deploy/tools/prepare_large_file.sh
@@ -1,13 +1,13 @@
-FILE2='cirros-0.3.4-x86_64-disk.img'
-if [ -f $FILE2 ]; then
-    echo "$FILE is already here"
-else
-    wget "http://download.cirros-cloud.net/0.3.4/$FILE2"
-fi
-
-FILE3='xenial-server-cloudimg-amd64-disk1.img'
-if [ -f $FILE3 ]; then
-    echo "$FILE is already here"
+FILE1='xenial-server-cloudimg-amd64-disk1.img'
+if [ -f $FILE1 ]; then
+    echo "$FILE1 is already here"
 else
     wget "http://cloud-images.ubuntu.com/xenial/current/$FILE3"
+fi
+
+FILE2='cirros-0.3.4-x86_64-disk.img'
+if [ -f $FILE2 ]; then
+    echo "$FILE2 is already here"
+else
+    wget "http://download.cirros-cloud.net/0.3.4/$FILE2"
 fi
