b22f98e9cbf0a233d2754b00c64ea6d4b4562bf1
Haoran Feng <haoran.feng@alo7.com>
2017-08-21 16:35:40 +0800

    brand new

--- 003-kolla-mul-deploy/tools/new_deploy.sh
+++ 003-kolla-mul-deploy/tools/new_deploy.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
-# first to clean up all the container
-/usr/share/kolla-ansible/tools/cleanup-host
-/usr/share/kolla-ansible/tools/cleanup-containers
-
-# brand new deploy
-kolla-ansible deploy -i /root/inventory/multinode
-
-kolla-ansible post-deploy
-source /etc/kolla/admin-openrc.sh
-/usr/share/kolla-ansible/init-runonce
+FILE='/etc/kolla/admin-openrc.sh'
+if [ -f $FILE ]; then
+    echo "$FILE is already there"
+else
+    kolla-ansible deploy -i /root/inventory/multinode
+    kolla-ansible post-deploy
+    source "$FILE"
+    /usr/share/kolla-ansible/init-runonce
+fi
