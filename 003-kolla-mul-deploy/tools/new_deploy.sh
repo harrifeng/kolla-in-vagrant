1a2bf61c24c13336feedad65005f85a5a1dfc7ab
Haoran Feng <haoran.feng@alo7.com>
2017-08-21 17:40:58 +0800

    no docker client on them

--- 003-kolla-mul-deploy/tools/new_deploy.sh
+++ 003-kolla-mul-deploy/tools/new_deploy.sh
@@ -5,8 +5,6 @@
 FILE='/etc/kolla/admin-openrc.sh'
 if [ -f $FILE ]; then
     echo "$FILE is already there"
-    /usr/share/kolla-ansible/tools/cleanup-host
-    /usr/share/kolla-ansible/tools/cleanup-containers
 else
     kolla-ansible post-deploy
     source "$FILE"
