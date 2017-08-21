2f590c36a1ed4d8a23665e6b727ce581bd85c7b1
Haoran Feng <haoran.feng@alo7.com>
2017-08-21 16:45:45 +0800

    new clean

--- 003-kolla-mul-deploy/tools/new_deploy.sh
+++ 003-kolla-mul-deploy/tools/new_deploy.sh
@@ -4,6 +4,8 @@ FILE='/etc/kolla/admin-openrc.sh'
 if [ -f $FILE ]; then
     echo "$FILE is already there"
 else
+    /usr/share/kolla-ansible/tools/cleanup-host
+    /usr/share/kolla-ansible/tools/cleanup-containers
     kolla-ansible deploy -i /root/inventory/multinode
     kolla-ansible post-deploy
     source "$FILE"
