819c1e0e793cb0e7e0bc61fb840a5b3225cb036e
Haoran Feng <haoran.feng@alo7.com>
2017-08-22 16:37:09 +0800

    ok I had this

--- 001-docker-repo/prepare_large_file.sh
+++ 001-docker-repo/prepare_large_file.sh
@@ -1,13 +1,11 @@
-FILE='docker-ce-17.06.0.ce-1.el7.centos.x86_64.rpm'
-if [ -f $FILE ]; then
-    echo "$FILE is already here"
-else
-    wget https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-17.06.0.ce-1.el7.centos.x86_64.rpm
-fi
+function download_if_not_here {
+    if [ -f "$1/$2" ]; then
+        echo "$1/$2 is already here"
+    else
+        wget -P "$1" "$3/$2"
+    fi
+}
 
-FILE2='centos-binary-registry-ocata.tar.gz'
-if [ -f $FILE2 ]; then
-    echo "$FILE2 is already here"
-else
-  wget http://tarballs.openstack.org/kolla/images/centos-binary-registry-ocata.tar.gz
-fi
+download_if_not_here 'httpd' 'docker-ce-17.06.0.ce-1.el7.centos.x86_64.rpm' 'https://download.docker.com/linux/centos/7/x86_64/stable/Packages'
+download_if_not_here 'httpd' 'python2-pip-8.1.2-5.el7.noarch.rpm' 'http://mirror.cs.pitt.edu/epel/7/x86_64/p'
+download_if_not_here 'httpd' 'centos-binary-registry-ocata.tar.gz' 'http://tarballs.openstack.org/kolla/images'
