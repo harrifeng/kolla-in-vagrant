1a249583f375173fb9a921ddaa1e9dae882c6c3e
Haoran Feng <haoran.feng@alo7.com>
2017-08-23 16:28:46 +0800

    fix 005 tool

--- 05-kolla-mul-network/tools/prepare_large_file.sh
+++
@@ -1,6 +0,0 @@
-FILE='docker-ce-17.06.0.ce-1.el7.centos.x86_64.rpm'
-if [ -f $FILE ]; then
-    echo "$FILE is already here"
-else
-    wget "https://download.docker.com/linux/centos/7/x86_64/stable/Packages/$FILE"
-fi
